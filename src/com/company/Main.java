8a9d9055f95979352c48adfbb2983a762ba372d4
Aarav Noronha <aarav.noronha@gmail.com>
2018-06-27 19:20:05 -0700

    longprefix implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // longprefix test
+        String[] A = new String[]{"flower","flow","flight", "fl"};
+        String S = longprefix(A);
+        System.out.printf("longprefix %s\n",S );
+    }
+
+    // e.g. ["flower","flow","flight"], outputs "fl"
+    public static String  longprefix(String [] A) {
+       int charnum = 0;
+       String prefix ="";
+       boolean done = false;
+       while(!done) {
+           String cmp = A[0].substring(charnum,charnum+1);
+           // check charnum-th character in all Strings from 1 to A.length
+           for(int i = 1; i < A.length; i++) {
+               if (charnum >= A[i].length()) {
+                   done = true;
+               } else {
+                   String cmp2 = A[i].substring(charnum, charnum + 1);
+                   if (!cmp.equals(cmp2)) {
+                       done = true;
+                   }
+               }
+           }
+           if(!done) {
+               prefix += cmp;
+           }
+           charnum++;
+       }
+       return prefix;
+
     }
 }
